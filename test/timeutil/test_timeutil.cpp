c5281777ac194089e39b99cf6882ce838806426f
hxk <374961015@qq.com>
2023-09-29 20:10:47 +0800

    change timeutil test

--- test/timeutil/test_timeutil.cpp
+++ test/timeutil/test_timeutil.cpp
@@ -1,15 +1,13 @@
-#include "code/util/TimeUtil.h"
+#include "code/util/TimeStamp.h"
 #include <iostream>
 #include <thread>
 int main()
 {
-    hxk::TimeUtil now;
-    std::cout << now.microSeconds() << now.getDay() << std::endl;
-    char temp[32];
-    size_t end = now.formatTime(temp);
-    temp[end] ='\0';
+    hxk::TimeStamp now;
+    std::cout << now.milliSeconds() << std::endl;
+    std::cout << now.microSeconds() << std::endl;
     std::cout << now.formatTime() << std::endl;
     std::this_thread::sleep_for(std::chrono::milliseconds(1000));
-    std::cout << now.microSeconds() << now.getDay() << std::endl;
-    std::cout << now.formatTime() << std::endl;
+
+    std::cout << now.now().formatTime() << std::endl;
 }
